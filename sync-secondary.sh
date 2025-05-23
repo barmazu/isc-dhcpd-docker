b04076d85cf9ddfe38f69e21500d1a08706c1d2d
barmazu <barmazu@gmail.com>
2025-05-23 21:24:34 +0200

    Enforce permissions via setfacl

--- sync-secondary.sh
+++ sync-secondary.sh
@@ -2,15 +2,17 @@
 
 set -eE
 
-#
-# Make sure pihole.toml is rw by system user, eg sudo setfacl -m u:user:rwx etc-pihole/pihole.toml
-#
-
 if ! command -v yq >/dev/null 2>&1; then
     echo "Error: yq command not found."
     echo "Make sure you have go-yq (https://github.com/mikefarah/yq/releases) in the PATH"
     exit 1
 fi
+
+#
+# Make sure pihole.toml is rw by system user, eg sudo setfacl -m u:user:rwx etc-pihole/pihole.toml
+#
+sudo setfacl -m u:pi:rwx etc-pihole/pihole.toml
+ssh pihole2 'sudo setfacl -m u:pi:rwx ~/etc-pihole/pihole.toml'
 
 # SYNC PIHOLE
 scp ~/etc-pihole/pihole.toml  pihole2:/home/pi/etc-pihole/pihole.toml
